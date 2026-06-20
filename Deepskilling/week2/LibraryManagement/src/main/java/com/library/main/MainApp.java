5d9f4d43dd4d6fddf948f0771073244c74e7a62d
ParagDevda <paragdevda@gmail.com>
2026-06-20 09:59:33 +0530

    configuring a basic spring application done

---
+++ Deepskilling/week2/LibraryManagement/src/main/java/com/library/main/MainApp.java
@@ -0,0 +1,21 @@
+package com.library.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+
+public class MainApp {
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+	    BookRepository br1 = (BookRepository) context.getBean("BR1");
+	    br1.displayBooks();
+	    
+	    BookService bs1 = (BookService) context.getBean("BS1");
+	    bs1.serviceMethod();
+	    
+	}
+}
---
+++ Deepskilling/week2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+
+	 public void serviceMethod() {
+	        System.out.println("BookService Bean Created");
+	    }
+}
