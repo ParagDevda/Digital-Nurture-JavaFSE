871b895f8e45740853eca52d7824577d9eb92b59
ParagDevda <paragdevda@gmail.com>
2026-07-01 14:29:06 +0530

    Hello_World_RESTful_Web_Service done

--- Deepskilling/week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Deepskilling/week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -8,6 +8,7 @@ public class SpringLearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		
 	}
 
 }
--- Deepskilling/week3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/HelloController.java
+++ Deepskilling/week3/spring-learn/src/main/java/com/cognizant/spring_learn/controller/HelloController.java
@@ -2,15 +2,15 @@ package com.cognizant.spring_learn.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
-public class controller {
+public class HelloController {
 
-    @GetMapping("/")
-    public String getMethodName() {
-        return "hello world";
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!!";
     }
     
 }
