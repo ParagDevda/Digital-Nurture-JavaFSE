aa08d72bfe9024527b17dd0c5ccb40e2b60d6488
ParagDevda <paragdevda@gmail.com>
2026-07-06 10:32:23 +0530

    Create_authentication_service_that_returns_JWT done

--- Deepskilling/week3/spring_jwt/src/main/java/com/cognizant/spring_jwt/controller/JwtController.java
+++ Deepskilling/week3/spring_jwt/src/main/java/com/cognizant/spring_jwt/controller/JwtController.java
@@ -1,2 +1,13 @@
-package com.cognizant.spring_jwt.controller;public class JwtController {
+package com.cognizant.spring_jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtController {
+
+    @GetMapping("/")
+    public  String hello(){
+        return "hello";
+    }
 }
