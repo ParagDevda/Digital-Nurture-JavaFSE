9af585253fe9b21197af346a40eb801f99eb1244
ParagDevda <paragdevda@gmail.com>
2026-07-18 00:16:36 +0530

    ReactJS_HOL-2 done

---
+++ Deepskilling/week5/ReactJS-HOL-2/student_app/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <Home/>
+      <Contact/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
